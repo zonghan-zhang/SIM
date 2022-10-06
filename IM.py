6f13941144050ab7848d4b48f3fcac79be581a55
oates9895 <45721436+oates9895@users.noreply.github.com>
2022-10-05 22:52:32 -0500

    Add files via upload

---
+++ IM.py
@@ -0,0 +1,691 @@
+import networkx as nx
+import numpy as np
+from simulation import simulationIC, simulationLT
+from score import SobolT
+import ndlib
+import ndlib.models.epidemics as ep
+import ndlib.models.ModelConfig as mc
+import statistics as s
+import heapdict as hd
+import random
+
+def eigen(g, config, budget):
+
+    g_eig = g.__class__()
+    g_eig.add_nodes_from(g)
+    g_eig.add_edges_from(g.edges)
+    for a, b in g_eig.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_eig[a][b]['weight'] = weight
+
+    eig = []
+
+    for k in range(budget):
+
+        eigen = nx.eigenvector_centrality_numpy(g_eig)
+        selected = sorted(eigen, key=eigen.get, reverse=True)[0]
+        eig.append(selected)
+        g_eig.remove_node(selected)
+
+    return eig
+
+def degree(g, config, budget):
+    g_deg = g.__class__()
+    g_deg.add_nodes_from(g)
+    g_deg.add_edges_from(g.edges)
+    for a, b in g_deg.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_deg[a][b]['weight'] = weight
+
+    deg = []
+
+    for k in range(budget):
+        degree = nx.centrality.degree_centrality(g_deg)
+        selected = sorted(degree, key=degree.get, reverse=True)[0]
+        deg.append(selected)
+        g_deg.remove_node(selected)
+
+    return deg
+
+def pi(g, config, budget):
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    result = []
+
+    for k in range(budget):
+
+        n = g_greedy.number_of_nodes()
+
+        I = np.ones((n, 1))
+
+        C = np.ones((n, n))
+        N = np.ones((n, n))
+
+        A = nx.to_numpy_matrix(g_greedy, nodelist=list(g_greedy.nodes()))
+
+        for i in range(5):
+            B = np.power(A, i + 1)
+            D = C - B
+            N = np.multiply(N, D)
+
+        P = C - N
+
+        pi = np.matmul(P, I)
+
+        value = {}
+
+        for i in range(n):
+            value[list(g_greedy.nodes())[i]] = pi[i, 0]
+
+        selected = sorted(value, key=value.get, reverse=True)[0]
+
+        result.append(selected)
+
+        g_greedy.remove_node(selected)
+
+    return result
+
+def sigma(g, config, budget):
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    result = []
+
+    for k in range(budget):
+
+        n = g_greedy.number_of_nodes()
+
+        I = np.ones((n, 1))
+
+        F = np.ones((n, n))
+        N = np.ones((n, n))
+
+        A = nx.to_numpy_matrix(g, nodelist=g_greedy.nodes())
+
+        sigma = I
+        for i in range(5):
+            B = np.power(A, i + 1)
+            C = np.matmul(B, I)
+            sigma += C
+
+        value = {}
+
+        for i in range(n):
+            value[list(g_greedy.nodes())[i]] = sigma[i, 0]
+
+        selected = sorted(value, key=value.get, reverse=True)[0]
+
+        result.append(selected)
+
+        g_greedy.remove_node(selected)
+
+    return result
+
+def Netshield(g, config, budget):
+
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    A = nx.adjacency_matrix(g_greedy)
+
+    lam, u = np.linalg.eigh(A.toarray())
+    lam = list(lam)
+    lam = lam[-1]
+
+    u = u[:, -1]
+
+    u = np.abs(np.real(u).flatten())
+    v = (2 * lam * np.ones(len(u))) * np.power(u, 2)
+
+    nodes = []
+    for i in range(budget):
+        B = A[:, nodes]
+        b = B * u[nodes]
+
+        score = v - 2 * b * u
+        score[nodes] = -1
+
+        nodes.append(np.argmax(score))
+
+    return nodes
+
+def Soboldeg(g, config, budget):
+    g_deg = g.__class__()
+    g_deg.add_nodes_from(g)
+    g_deg.add_edges_from(g.edges)
+    for a, b in g_deg.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_deg[a][b]['weight'] = weight
+
+    deg = []
+
+    for k in range(2*budget):
+        degree = nx.centrality.degree_centrality(g_deg)
+        selected = sorted(degree, key=degree.get, reverse=True)[0]
+        deg.append(selected)
+        g_deg.remove_node(selected)
+
+
+    for j in range(budget):
+        df = simulationIC(1, g, deg, config)
+        ST = SobolT(df, deg)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        deg.remove((rem))
+
+    return deg
+
+def degreeDis(g, config, budget):
+
+    selected = []
+    d = {}
+    t = {}
+    dd = hd.heapdict()
+
+    for node in g.nodes():
+        d[node] = sum([g[node][v]['weight'] for v in g[node]])
+        dd[node] = -d[node]
+        t[node] = 0
+
+    for i in range(budget):
+        seed, _ = dd.popitem()
+        selected.append(seed)
+        for v in g.neighbors(seed):
+            if v not in selected:
+                t[v] += g[seed][v]['weight']
+                discount = d[v] - 2*t[v] - (d[v] - t[v])*t[v]
+                dd[v] = -discount
+
+    return selected
+
+def SoboldegreeDis(g, config, budget):
+
+    selected = []
+    d = {}
+    t = {}
+    dd = hd.heapdict()
+
+    for node in g.nodes():
+        d[node] = sum([g[node][v]['weight'] for v in g[node]])
+        dd[node] = -d[node]
+        t[node] = 0
+
+    for i in range(2*budget):
+        seed, _ = dd.popitem()
+        selected.append(seed)
+        for v in g.neighbors(seed):
+            if v not in selected:
+                t[v] += g[seed][v]['weight']
+                discount = d[v] - 2*t[v] - (d[v] - t[v])*t[v]
+                dd[v] = -discount
+
+    for j in range(budget):
+        df = simulationIC(1, g, selected, config)
+        ST = SobolT(df, selected)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        selected.remove((rem))
+
+    return selected
+
+def greedyIC(g, config, budget):
+
+    selected = []
+    candidates = []
+
+    for node in g.nodes():
+        candidates.append(node)
+
+    for i in range(budget):
+        max = 0
+        index = -1
+        for node in candidates:
+            seed = []
+            for item in selected:
+                seed.append(item)
+            seed.append(node)
+
+            # g_temp = g.__class__()
+            # g_temp.add_nodes_from(g)
+            # g_temp.add_edges_from(g.edges)
+            result = []
+
+            for iter in range(100):
+
+                model_temp = ep.IndependentCascadesModel(g) # _temp
+                config_temp = mc.Configuration()
+                config_temp.add_model_initial_configuration('Infected', seed)
+
+                for a, b in g.edges(): # _temp
+                    weight = config.config["edges"]['threshold'][(a, b)]
+                    # g_temp[a][b]['weight'] = weight
+                    config_temp.add_edge_configuration('threshold', (a, b), weight)
+
+                model_temp.set_initial_status(config_temp)
+
+                iterations = model_temp.iteration_bunch(5)
+
+                total_no = 0
+
+                for j in range(5):
+                    a = iterations[j]['node_count'][1]
+                    total_no += a
+
+                result.append(total_no)
+
+            if s.mean(result) > max:
+                max = s.mean(result)
+                index = node
+
+        selected.append(index)
+        candidates.remove(index)
+
+    return selected
+
+def greedyLT(g, config, budget):
+
+    selected = []
+    candidates = []
+
+    for node in g.nodes():
+        candidates.append(node)
+
+    for i in range(budget):
+        max = 0
+        index = -1
+        for node in candidates:
+            seed = []
+            for item in selected:
+                seed.append(item)
+            seed.append(node)
+
+            # g_temp = g.__class__()
+            # g_temp.add_nodes_from(g)
+            # g_temp.add_edges_from(g.edges)
+            result = []
+
+            for iter in range(100):
+
+                model_temp = ep.ThresholdModel(g) # _temp
+                config_temp = mc.Configuration()
+                config_temp.add_model_initial_configuration('Infected', seed)
+
+                for a, b in g.edges(): # _temp
+                    weight = config.config["edges"]['threshold'][(a, b)]
+                    # g_temp[a][b]['weight'] = weight
+                    config_temp.add_edge_configuration('threshold', (a, b), weight)
+
+                for i in g.nodes():
+                    threshold = random.randrange(1, 20)
+                    threshold = round(threshold / 100, 2)
+                    config_temp.add_node_configuration("threshold", i, threshold)
+
+                model_temp.set_initial_status(config_temp)
+
+                iterations = model_temp.iteration_bunch(5)
+
+                total_no = iterations[4]['node_count'][1]
+                input.append(total_no)
+
+            if s.mean(result) > max:
+                max = s.mean(result)
+                index = node
+
+        selected.append(index)
+        candidates.remove(index)
+
+    return selected
+
+'''
+def SobolIM2(g, config):
+    g_deg = g.__class__()
+    g_deg.add_nodes_from(g)
+    g_deg.add_edges_from(g.edges)
+    for a, b in g_deg.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_deg[a][b]['weight'] = weight
+
+    deg = []
+
+    for k in range(10):
+        degree = nx.centrality.degree_centrality(g_deg)
+        selected = sorted(degree, key=degree.get, reverse=True)[0]
+        deg.append(selected)
+        g_deg.remove_node(selected)
+
+
+    df = simulationIC(1, g, deg, config)
+    ST = STS(df, deg)
+    rank = []
+    for node in sorted(ST, key=ST.get, reverse=True):
+        rank.append(node)
+    rem = rank.pop()
+    nodes = rem.split('.')
+    k = len(nodes)
+    for i in range(k-1):
+        deg.remove(int(nodes[i]))
+
+    return deg
+
+def SobolIM(g, config):
+    g_deg = g.__class__()
+    g_deg.add_nodes_from(g)
+    g_deg.add_edges_from(g.edges)
+    for a, b in g_deg.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_deg[a][b]['weight'] = weight
+
+    deg = []
+
+    for k in range(10):
+        degree = nx.centrality.degree_centrality(g_deg)
+        selected = sorted(degree, key=degree.get, reverse=True)[0]
+        deg.append(selected)
+        g_deg.remove_node(selected)
+
+
+    for j in range(5):
+        df = simulationIC(1, g, deg, config)
+        ST = SobolT(df, deg)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        deg.remove((rem))
+
+    return deg
+'''
+def Soboleigen(g, config, budget):
+    g_eig = g.__class__()
+    g_eig.add_nodes_from(g)
+    g_eig.add_edges_from(g.edges)
+    for a, b in g_eig.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_eig[a][b]['weight'] = weight
+
+    eig = []
+
+    for k in range(2*budget):
+
+        eigen = nx.eigenvector_centrality_numpy(g_eig)
+        selected = sorted(eigen, key=eigen.get, reverse=True)[0]
+        eig.append(selected)
+        g_eig.remove_node(selected)
+
+
+    for j in range(budget):
+        df = simulationIC(1, g, eig, config)
+        ST = SobolT(df, eig)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        eig.remove((rem))
+
+    return eig
+
+def SobolPi(g, config, budget):
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    result = []
+
+    for k in range(2*budget):
+
+        n = g_greedy.number_of_nodes()
+
+        I = np.ones((n, 1))
+
+        C = np.ones((n, n))
+        N = np.ones((n, n))
+
+        A = nx.to_numpy_matrix(g_greedy, nodelist=list(g_greedy.nodes()))
+
+        for i in range(5):
+            B = np.power(A, i + 1)
+            D = C - B
+            N = np.multiply(N, D)
+
+        P = C - N
+
+        pi = np.matmul(P, I)
+
+        value = {}
+
+        for i in range(n):
+            value[list(g_greedy.nodes())[i]] = pi[i, 0]
+
+        selected = sorted(value, key=value.get, reverse=True)[0]
+
+        result.append(selected)
+
+        g_greedy.remove_node(selected)
+
+
+    for j in range(budget):
+        df = simulationIC(1, g, result, config)
+        ST = SobolT(df, result)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        result.remove((rem))
+
+    return result
+
+def SobolSigma(g, config, budget):
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    result = []
+
+    for k in range(2*budget):
+
+        n = g_greedy.number_of_nodes()
+
+        I = np.ones((n, 1))
+
+        F = np.ones((n, n))
+        N = np.ones((n, n))
+
+        A = nx.to_numpy_matrix(g, nodelist=g_greedy.nodes())
+
+        sigma = I
+        for i in range(5):
+            B = np.power(A, i + 1)
+            C = np.matmul(B, I)
+            sigma += C
+
+        value = {}
+
+        for i in range(n):
+            value[list(g_greedy.nodes())[i]] = sigma[i, 0]
+
+        selected = sorted(value, key=value.get, reverse=True)[0]
+
+        result.append(selected)
+
+        g_greedy.remove_node(selected)
+
+
+    for j in range(budget):
+        df = simulationIC(1, g, result, config)
+        ST = SobolT(df, result)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        result.remove((rem))
+
+    return result
+
+def SobolNS(g, config, budget):
+
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    A = nx.adjacency_matrix(g_greedy)
+
+    lam, u = np.linalg.eigh(A.toarray())
+    lam = list(lam)
+    lam = lam[-1]
+
+    u = u[:, -1]
+
+    u = np.abs(np.real(u).flatten())
+    v = (2 * lam * np.ones(len(u))) * np.power(u, 2)
+
+    nodes = []
+    for i in range(2*budget):
+        B = A[:, nodes]
+        b = B * u[nodes]
+
+        score = v - 2 * b * u
+        score[nodes] = -1
+
+        nodes.append(np.argmax(score))
+
+
+    for j in range(budget):
+        df = simulationIC(1, g, nodes, config)
+        ST = SobolT(df, nodes)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        nodes.remove((rem))
+
+    return nodes
+
+def SoboldegLT(g, config, budget):
+    g_deg = g.__class__()
+    g_deg.add_nodes_from(g)
+    g_deg.add_edges_from(g.edges)
+    for a, b in g_deg.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_deg[a][b]['weight'] = weight
+
+    deg = []
+
+    for k in range(2*budget):
+        degree = nx.centrality.degree_centrality(g_deg)
+        selected = sorted(degree, key=degree.get, reverse=True)[0]
+        deg.append(selected)
+        g_deg.remove_node(selected)
+
+    for j in range(budget):
+        df = simulationLT(1, g, deg, config)
+        ST = SobolT(df, deg)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        deg.remove((rem))
+
+    return deg
+
+def SoboleigenLT(g, config,budget):
+    g_eig = g.__class__()
+    g_eig.add_nodes_from(g)
+    g_eig.add_edges_from(g.edges)
+    for a, b in g_eig.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_eig[a][b]['weight'] = weight
+
+    eig = []
+
+    for k in range(2*budget):
+
+        eigen = nx.eigenvector_centrality_numpy(g_eig)
+        selected = sorted(eigen, key=eigen.get, reverse=True)[0]
+        eig.append(selected)
+        g_eig.remove_node(selected)
+
+
+    for j in range(budget):
+        df = simulationLT(1, g, eig, config)
+        ST = SobolT(df, eig)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        eig.remove((rem))
+
+    return eig
+
+def SobolNSLT(g, config,budget):
+
+    g_greedy = g.__class__()
+    g_greedy.add_nodes_from(g)
+    g_greedy.add_edges_from(g.edges)
+
+    for a, b in g_greedy.edges():
+        weight = config.config["edges"]['threshold'][(a, b)]
+        g_greedy[a][b]['weight'] = weight
+
+    A = nx.adjacency_matrix(g_greedy)
+
+    lam, u = np.linalg.eigh(A.toarray())
+    lam = list(lam)
+    lam = lam[-1]
+
+    u = u[:, -1]
+
+    u = np.abs(np.real(u).flatten())
+    v = (2 * lam * np.ones(len(u))) * np.power(u, 2)
+
+    nodes = []
+    for i in range(2*budget):
+        B = A[:, nodes]
+        b = B * u[nodes]
+
+        score = v - 2 * b * u
+        score[nodes] = -1
+
+        nodes.append(np.argmax(score))
+
+    for j in range(budget):
+        df = simulationLT(1, g, nodes, config)
+        ST = SobolT(df, nodes)
+        rank = []
+        for node in sorted(ST, key=ST.get, reverse=True):
+            rank.append(node)
+        rem = rank.pop()
+        nodes.remove((rem))
+
+    return nodes
---
+++ case-study.py
@@ -0,0 +1,128 @@
+import networkx as nx
+from time import time
+
+from graphGeneration import Cora, CiteSeer, PubMed, connSW, ER
+from IM import eigen, degree, pi, sigma, Netshield
+from score import scoreIC, Y, SobolT, sobols, IE
+from simulation import simulationIC, simulationLT
+import statistics as s
+
+g, config = Cora()
+# g, config = CiteSeer()
+# g, config = PubMed()
+# g, config = ER()
+# g, config = connSW()
+
+print("Graph is on.")
+print(nx.info(g))
+
+print('------------------------------------------------')
+print('degree')
+start = time()
+set = degree(g,config,5)
+end = time()
+print('time: ', end - start)
+print('degree: ', set)
+
+# Distance among seeds
+for i in range(len(set)-1):
+  for j in range(i+1, len(set)):
+    print(set[i],'-',set[j],':',nx.shortest_path_length(g, source = set[i], target = set[j]))
+
+# Sobol Total of each seed
+df = simulationIC(100,g,set,config)
+print(df)
+EY,VY = Y(df)
+print('Expectation: ', EY)
+print('Variance: ', VY)
+
+Sobols = sobols(df,set)
+for key in Sobols:
+  print(key, ' : ', Sobols[key])
+
+ST = SobolT(df,set)
+for node in ST.keys():
+  print(node, ': ', ST[node]/VY)
+
+
+
+ie, std = IE(df,set)
+print('IE:', ie)
+
+# Marginal Contribution
+MC0 = []
+MC1 = []
+MC2 = []
+MC3 = []
+MC4 = []
+ST0 = []
+ST1 = []
+ST2 = []
+ST3 = []
+ST4 = []
+for i in range(5):
+
+  df = simulationIC(100, g, set, config)
+  ie, std = IE(df, set)
+  EY, VY = Y(df)
+  ST = SobolT(df, set)
+
+  node0 = set[0]
+  ST0.append(ST[node0] / VY)
+  node1 = set[1]
+  ST1.append(ST[node1] / VY)
+  node2 = set[2]
+  ST2.append(ST[node2] / VY)
+  node3 = set[3]
+  ST3.append(ST[node3] / VY)
+  node4 = set[4]
+  ST4.append(ST[node4] / VY)
+  # set[0]
+  sim = df[(df[set[0]] == 0) & (df[set[1]] == 1) & (df[set[2]] == 1) & (df[set[3]] == 1) & (df[set[4]] == 1)]
+  Esim = s.mean(sim['result']) - ie
+  MC0.append(Esim)
+
+  # set[1]
+
+  sim = df[(df[set[0]] == 1) & (df[set[1]] == 0) & (df[set[2]] == 1) & (df[set[3]] == 1) & (df[set[4]] == 1)]
+  Esim = s.mean(sim['result']) - ie
+  MC1.append(Esim)
+
+  # set[2]
+
+  sim = df[(df[set[0]] == 1) & (df[set[1]] == 1) & (df[set[2]] == 0) & (df[set[3]] == 1) & (df[set[4]] == 1)]
+  Esim = s.mean(sim['result']) - ie
+  MC2.append(Esim)
+
+  # set[3]
+
+  sim = df[(df[set[0]] == 1) & (df[set[1]] == 1) & (df[set[2]] == 1) & (df[set[3]] == 0) & (df[set[4]] == 1)]
+  Esim = s.mean(sim['result']) - ie
+  MC3.append(Esim)
+
+  # set[4]
+
+  sim = df[(df[set[0]] == 1) & (df[set[1]] == 1) & (df[set[2]] == 1) & (df[set[3]] == 1) & (df[set[4]] == 0)]
+  Esim = s.mean(sim['result']) - ie
+  MC4.append(Esim)
+
+print('degree: ', set)
+print(s.mean(MC0), ' +- ', s.stdev(MC0))
+print(s.mean(MC1), ' +- ', s.stdev(MC1))
+print(s.mean(MC2), ' +- ', s.stdev(MC2))
+print(s.mean(MC3), ' +- ', s.stdev(MC3))
+print(s.mean(MC4), ' +- ', s.stdev(MC4))
+print("ST-----------------------------")
+print(s.mean(ST0), ' +- ', s.stdev(ST0))
+print(s.mean(ST1), ' +- ', s.stdev(ST1))
+print(s.mean(ST2), ' +- ', s.stdev(ST2))
+print(s.mean(ST3), ' +- ', s.stdev(ST3))
+print(s.mean(ST4), ' +- ', s.stdev(ST4))
+
+rank = []
+for node in sorted(ST, key=ST.get, reverse = True):
+  rank.append(node)
+print(rank)
+
+
+
